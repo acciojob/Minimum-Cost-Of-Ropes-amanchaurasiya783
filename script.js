d0a6fb59ff6054acbabfa9e2602fae069f8ea5c2
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-07-28 12:10:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+	const rope = document.getElementById("rope-lengths").value;
+	let ropeArray = Array.from(rope);
+	let sortedArr = ropeArray.sort((a,b) =>{return a-b;});
+	let cost = 0;
+	let n = sortedArr.length;
+	while (n != 1) {
+		for(let i=0; i<n; i++){
+		cost += sortedArr[i];
+	}
+	n--
+	}
+	document.getElementById("result").innerHTML = cost;
 }  
